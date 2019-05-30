c7f82f0e2060a523abb948c0923ba80c46fe8b15
sv615218 <sv615218@gmail.com>
2019-05-30 15:23:07 +0530

    5 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { MatToolbarModule } from '@angular/material/toolbar';
+ 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { MatIconModule } from '@angular/material/icon';
+ 
 @NgModule({
-  imports: [BrowserModule, FormsModule, MatIconModule, MatToolbarModule],
+  imports: [BrowserModule, FormsModule, ],
   declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent]
 })
